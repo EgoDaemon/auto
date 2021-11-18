15acd3fb9d77881406a5fb67f114bbd6b03e6009
Alexandra Messner <54995607+EgoDaemon@users.noreply.github.com>
2021-11-18 16:12:59 +0300

    MISC
    репорт конф., гарантии селок и пр. все из MISC кроме MART_AUTH части

---
+++ A_MISC_part1_BQ.py
@@ -0,0 +1,205 @@
+from ga_connector import ga_connect
+from sqlalchemy import create_engine
+import datetime
+import pandas
+from pandas import DataFrame as pd
+from pandas import DataFrame as df
+import pandas_gbq
+from ga_connector import ga_connect
+from sqlalchemy import create_engine
+import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2 import service_account
+
+key_path = '/home/web_analytics/m2-main-cd9ed0b4e222.json'
+gbq_credential = service_account.Credentials.from_service_account_file(key_path,)
+
+def date_pairs(date1, date2, step= 1):
+    pairs= []
+    while date2 > date1:
+        prev_date = date2 - datetime.timedelta(days=step-1) if date2 - datetime.timedelta(days=step) > date1 else date1
+        pair = [str(prev_date), str(date2)]   
+        date2 -= datetime.timedelta(days=step)
+        pairs.append(pair)
+    pairs.reverse()
+    return pairs
+
+def get_df(query): 
+    engine = create_engine(key_path)
+    df_check = engine.execute(query)
+    df_q = pd(df_check.fetchall(), columns = df_check.keys())
+    engine.dispose()
+    return df_q
+
+
+def get_geo(page):    
+    msk='moskva-i-oblast|moskva|moskovskaya-oblast'.split('|')
+    spb= 'sankt-peterburg|leningradskaya-oblast|sankt-peterburg-i-oblast'.split('|')
+    for g in msk:
+        if g in page:
+            return "MSK"
+    for g in spb:
+        if g in page:
+            return "SPB"
+    
+def purify_df(df):
+    df['landingpagepath'] = df['landingpagepath'].apply(get_geo)
+    return df
+
+start_date_tr = """SELECT  MAX(DATE) as date FROM `m2-main.UA_REPORTS.MISC_Transfer_broker` """
+start_date_tr  = pandas_gbq.read_gbq(start_date_tr, project_id='m2-main', credentials=gbq_credential)
+start_date_tr  = (datetime.datetime.strptime(start_date_tr['date'][0],"%Y-%m-%d").date()+datetime.timedelta(days=1))     
+
+date1 = start_date_tr
+end = (datetime.datetime.today() -datetime.timedelta(days=1)).date()
+dates_couples = date_pairs(date1, end, step=1)
+
+
+ga_conc = ga_connect('208464364')
+
+params = {'dimetions': [{'name': 'ga:isoweek'},
+                        {'name': 'ga:date'},
+                        {'name': 'ga:medium'},
+                        {'name': 'ga:source'},
+                        {'name': 'ga:dimension1'},
+                        
+                        {'name': 'ga:landingpagepath'}                        
+                       ],
+        'metrics':[{'expression': 'ga:totalevents'},
+                  ],
+        'filters':'ga:eventlabel=~^(ClCardSellMortgageClick|ClSerpSellMortgageClick)$;ga:landingpagepath=~/nedvizhimost;ga:source!~ohio8.vchecks.me'
+        }
+
+gotobro_source = ga_conc.report_pd(dates_couples,params)
+
+
+params = {'dimetions': [{'name': 'ga:isoweek'},
+                        {'name': 'ga:date'},
+                        {'name': 'ga:medium'},
+                        {'name': 'ga:source'},
+                        {'name': 'ga:eventlabel'},
+                        {'name': 'ga:eventaction'},
+                        
+                        {'name': 'ga:landingpagepath'} 
+                       ],
+        'metrics':[{'expression': 'ga:totalevents'},
+                  ],
+        'filters':'ga:eventlabel=~^claim_completed;ga:landingpagepath=~/nedvizhimost'
+        }
+
+lead_bro_source = ga_conc.report_pd(dates_couples,params)
+
+
+params = {'dimetions': [{'name': 'ga:isoweek'},
+                        {'name': 'ga:date'},
+                        {'name': 'ga:medium'},
+                        {'name': 'ga:source'},
+                        {'name': 'ga:eventlabel'}
+                       ],
+        'metrics':[{'expression': 'ga:totalevents'},
+                  ],
+        'filters':'ga:eventlabel=~SendFormRegSuccess;ga:source!~ohio8.vchecks.me'
+        }
+
+reg_all = ga_conc.report_pd(dates_couples,params)
+
+params = {'dimetions': [{'name': 'ga:isoweek'},
+                        {'name': 'ga:date'},
+                        {'name': 'ga:medium'},
+                        {'name': 'ga:source'},
+                        {'name': 'ga:eventlabel'}
+                       ],
+        'metrics':[{'expression': 'ga:totalevents'},
+                  ],
+        'filters':'ga:eventlabel=~SendFormRegSuccess;ga:landingpagepath=~/nedvizhimost;ga:source!~ohio8.vchecks.me'
+        }
+
+reg_up = ga_conc.report_pd(dates_couples,params)
+
+gotobro_source.to_gbq(f'UA_REPORTS.MISC_Transfer_broker', project_id='m2-main', if_exists='append', credentials=gbq_credential)
+lead_bro_source.to_gbq(f'UA_REPORTS.MISC_Leads_broker', project_id='m2-main', if_exists='append', credentials=gbq_credential)
+reg_all.to_gbq(f'UA_REPORTS.MISC_Registration_All', project_id='m2-main', if_exists='append', credentials=gbq_credential)
+reg_up.to_gbq(f'UA_REPORTS.MISC_Registration_UP', project_id='m2-main', if_exists='append', credentials=gbq_credential)
+
+a = '''SELECT * FROM m2-main.UA_REPORTS.MISC_Transfer_broker'''
+b = '''SELECT * FROM m2-main.UA_REPORTS.MISC_Leads_broker'''
+c = '''SELECT * FROM m2-main.UA_REPORTS.MISC_Registration_All'''
+d = '''SELECT * FROM m2-main.UA_REPORTS.MISC_Registration_UP'''
+
+
+gotobro_sql = pandas_gbq.read_gbq(a, project_id='m2-main', credentials=gbq_credential)
+lead_bro_sql = pandas_gbq.read_gbq(b, project_id='m2-main', credentials=gbq_credential)
+reg_all = pandas_gbq.read_gbq(c, project_id='m2-main', credentials=gbq_credential)
+reg_up = pandas_gbq.read_gbq(d, project_id='m2-main', credentials=gbq_credential)
+
+gotobro = purify_df(gotobro_sql)
+lead_bro = purify_df(lead_bro_sql)
+
+gotobro['isoweek'] = gotobro['isoweek'].astype(int)
+#gotobro['date'] = pd.to_datetime(gotobro['date'], format='%Y-%m-%d')
+gotobro['date'] = gotobro['date'].astype(str)
+gotobro['medium'] = gotobro['medium'].astype(str)
+gotobro['source'] = gotobro['source'].astype(str)
+gotobro['dimension1'] = gotobro['dimension1'].astype(str)
+gotobro['landingpagepath'] = gotobro['landingpagepath'].astype(str)
+gotobro['totalevents'] = gotobro['totalevents'].astype(int)
+
+lead_bro['isoweek'] = lead_bro['isoweek'].astype(int)
+#lead_bro['date'] = pd.to_datetime(lead_bro['date'], format='%Y-%m-%d')
+lead_bro['date'] = lead_bro['date'].astype(str)
+lead_bro['medium'] = lead_bro['medium'].astype(str)
+lead_bro['source'] = lead_bro['source'].astype(str)
+lead_bro['eventlabel'] = lead_bro['eventlabel'].astype(str)
+lead_bro['eventaction'] = lead_bro['eventaction'].astype(str)
+lead_bro['landingpagepath'] = lead_bro['landingpagepath'].astype(str)
+lead_bro['totalevents'] = lead_bro['totalevents'].astype(int)
+
+reg_all['isoweek'] = reg_all['isoweek'].astype(int)
+#reg_all['date'] = pd.to_datetime(reg_all['date'], format='%Y-%m-%d')
+reg_all['date'] = reg_all['date'].astype(str)
+reg_all['medium'] = reg_all['medium'].astype(str)
+reg_all['source'] = reg_all['source'].astype(str)
+reg_all['eventlabel'] = reg_all['eventlabel'].astype(str)
+reg_all['totalevents'] = reg_all['totalevents'].astype(int)
+
+reg_up['isoweek'] = reg_up['isoweek'].astype(int)
+#reg_up['date'] = pd.to_datetime(reg_up['date'], format='%Y-%m-%d')
+reg_up['date'] = reg_up['date'].astype(str)
+reg_up['medium'] = reg_up['medium'].astype(str)
+reg_up['source'] = reg_up['source'].astype(str)
+reg_up['eventlabel'] = reg_up['eventlabel'].astype(str)
+reg_up['totalevents'] = reg_up['totalevents'].astype(int)
+
+gb = gotobro.groupby(['isoweek','landingpagepath']).agg(totalevents=('totalevents', 'sum')).reset_index().replace('None', 'REG')
+gb_unique = gotobro.groupby(['isoweek','landingpagepath']).agg(dimension1=('dimension1', 'count')).reset_index().replace('None', 'REG')
+ld = lead_bro.groupby(['isoweek','landingpagepath']).agg(totalevents=('totalevents', 'sum')).reset_index().replace('None', 'REG')
+rg = reg_all.groupby(['isoweek']).agg(totalevents=('totalevents', 'sum')).reset_index()
+rg_up = reg_up.groupby(['isoweek']).agg(totalevents=('totalevents', 'sum')).reset_index()
+
+all_bro = [gb.columns.values.tolist()] + gb.values.tolist()
+all_bro_unique = [gb_unique.columns.values.tolist()] + gb_unique.values.tolist()
+all_bro_lead = [ld.columns.values.tolist()] + ld.values.tolist()
+regs_all = [rg.columns.values.tolist()] + rg.values.tolist()
+regs_up = [rg_up.columns.values.tolist()] + rg_up.values.tolist()
+
+from apiclient.discovery import build
+from google.oauth2 import service_account
+from oauth2client.service_account import ServiceAccountCredentials
+import gspread
+
+
+SCOPES = ['https://www.googleapis.com/auth/analytics.readonly',
+             'https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+g_credentials = ServiceAccountCredentials.from_json_keyfile_name(key_path, SCOPES)
+gc = gspread.authorize(g_credentials)
+
+
+sh = gc.open("1. РК Траффик 2020/21 Total")
+wk = sh.worksheet('report_conf')
+
+wk.update('A2', all_bro) # Переход в брокер
+wk.update('E2', all_bro_unique) #  Пользователи в брокер
+wk.update('H2', all_bro_lead) # Лиды ИБ
+wk.update('L2', regs_all) # Регистрация Всего
+wk.update('O2', regs_up) # Регистрации УП
---
+++ A_MISC_part2_BQ.py
@@ -0,0 +1,316 @@
+from ga_connector import ga_connect
+from sqlalchemy import create_engine
+import datetime
+import pandas
+from pandas import DataFrame as pd
+from pandas import DataFrame as df
+import pandas_gbq
+import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+import gspread
+
+key_path = '/home/web_analytics/m2-main-cd9ed0b4e222.json'
+gbq_credential = service_account.Credentials.from_service_account_file(key_path,)
+
+
+def date_pairs(date1, date2, step= 1):
+    pairs= []
+    while date2 > date1:
+        prev_date = date2 - datetime.timedelta(days=step-1) if date2 - datetime.timedelta(days=step) > date1 else date1
+        pair = [str(prev_date), str(date2)]   
+        date2 -= datetime.timedelta(days=step)
+        pairs.append(pair)
+    pairs.reverse()
+    return pairs
+
+def get_df(query): 
+    engine = create_engine(postrges_key)
+    df_check = engine.execute(query)
+    df_q = pd(df_check.fetchall(), columns = df_check.keys())
+    engine.dispose()
+    return df_q
+
+def get_df_new(query):
+    df_check = engine.execute(query)
+    return df(df_check.fetchall(), columns = df_check.keys()).fillna(0)
+
+def get_cpc_soures(cpms):
+    if 'second' in cpms:
+        return 'УП реклама'
+    if 'new-building' in cpms:
+        return 'Новосторойки реклама'
+    if cpms == '(not set)':
+        return 'Не рекламные'
+    else:
+        return 'Другие рекламные кампании'
+    
+def get_location(s):
+    if 'serp' in s.lower():
+        return 'SERP'
+    if 'card' in s.lower():
+        return 'CARD'
+    else:
+        return None 
+    
+def get_product(s):
+    if 'protection' in s.lower() or 'guarantee' in s.lower():
+        return 'protection'
+    if 'onlinebuy' in s.lower():
+        return 'onlinebuy'
+    else:
+        return None
+    
+def get_type(s):
+    
+    if 'click' in s.lower():
+        return 'click'
+    if 'view' in s.lower():
+        return 'view'
+    else:
+        return None  
+
+def get_geo(page):
+    msk='moskva-i-oblast|moskva|moskovskaya-oblast'.split('|')
+    spb= 'sankt-peterburg|leningradskaya-oblast|sankt-peterburg-i-oblast'.split('|')
+    for g in msk:
+        if g in page:
+            return "MSK"
+    for g in spb:
+        if g in page:
+            return "SPB"
+        
+def get_df(query):
+    df_check = engine.execute(query)
+    return df(df_check.fetchall(), columns = df_check.keys()).fillna(0)
+
+def purify_df(df):
+    df.columns = [i.replace('ga:', '').lower() for i in df.columns]
+    df['product'] = df['eventlabel'].apply(get_product)
+    df['card_serp'] = df['eventlabel'].apply(get_location)
+    df['action_type'] = df['eventlabel'].apply(get_type)
+    df = df.drop(columns = ['eventcategory','eventaction'])
+    df['totalevents'] = df['totalevents'].astype(int)
+    df['users'] = df['users'].astype(int)
+    df = df[df['product'].apply(lambda x: x is not None)]
+    return df
+
+
+q = """SELECT  MAX(date) FROM `m2-main.UA_REPORTS.MISC_UP_Banners` """
+start_D = pandas_gbq.read_gbq(q, project_id='m2-main', credentials=gbq_credential)
+date1 = (datetime.datetime.strptime(start_D['date'][0],"%Y-%m-%d").date()+datetime.timedelta(days=1))
+end = (datetime.datetime.today()-datetime.timedelta(days=1)).date()
+
+dates_couples = date_pairs(date1,end,step=1)
+dates_couples
+
+ga_conc = ga_connect('208464364')
+params = {'dimetions': [{'name': 'ga:isoweek'},
+                        {'name': 'ga:date'},
+                        {'name': 'ga:eventLabel'},
+                        {'name': 'ga:eventCategory'},
+                        
+                        {'name': 'ga:eventAction'}
+                       ],
+          
+        'metrics':[{'expression': 'ga:totalevents'},
+                    {'expression': 'ga:users'}
+                  ],
+          
+        
+        'filters':'ga:pagepath=~/nedvizhimost;ga:eventLabel=~banner'
+        }
+
+UP_rep = ga_conc.report_pd(dates_couples,params)
+
+
+UP_rep.to_gbq(f'UA_REPORTS.MISC_UP_Banners', project_id='m2-main', if_exists='append', credentials=gbq_credential)
+
+
+a = '''SELECT * FROM m2-main.UA_REPORTS.MISC_UP_Banners'''
+aa = pandas_gbq.read_gbq(a, project_id='m2-main', credentials=gbq_credential)
+event_banner_df = purify_df(aa)
+
+engine = create_engine('sqlite://')
+
+event_banner_df.to_sql("banners", con=engine, if_exists = 'replace', index = False)
+q = '''
+select 
+    isoweek,
+    product,
+    action_type,
+    card_serp,
+    sum(totalevents) as bnrs,
+    sum(users) as un_users
+from 
+    banners
+
+group by 1,2,3,4
+order by 1
+'''
+get_bans = get_df_new(q)
+
+
+
+
+SCOPES = ['https://www.googleapis.com/auth/analytics.readonly',
+             'https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+g_credentials = ServiceAccountCredentials.from_json_keyfile_name(key_path, SCOPES)
+
+
+gc = gspread.authorize(g_credentials)
+
+rows  = [list([get_bans.columns][0])]
+
+for i in get_bans.itertuples():
+    
+    if i.isoweek == 0:
+        continue
+    else:
+        ls=list(i)[1:]
+        rows.append(ls)
+
+banners_list = rows
+        
+sh = gc.open("1. РК Траффик 2020/21 Total")
+wk = sh.worksheet('source_all')
+wk.update('Y2',banners_list)
+
+# ГАРАНТИЯ СДЕЛОК И ПР.
+
+date1 = datetime.date(2021,8,9) # НЕ менять
+end = datetime.date(2021,11,14)
+dates_couples = date_pairs(date1,end, step=1)
+print(dates_couples)
+
+ga_conc = ga_connect('208464364')
+
+params = {'dimetions': [{'name': 'ga:isoweek'},
+                        {'name': 'ga:eventLabel'},
+                        {'name': 'ga:eventCategory'},
+                        {'name': 'ga:dimension1'}
+                       ],
+        'metrics':[{'expression': 'ga:totalevents'},
+                    {'expression': 'ga:users'}
+                  ],
+        'filters':'ga:landingpagepath=~nedvizhimost;ga:eventLabel=~lk_onlinebuy_lp_header_buy_click'
+        }
+
+od_lk_banners = ga_conc.report_pd(dates_couples,params)
+
+params = {'dimetions': [{'name': 'ga:isoweek'},
+                        {'name': 'ga:pagepath'},
+                        {'name': 'ga:dimension1'},
+                        {'name': 'ga:dimension2'}
+                       ],
+        'metrics':[
+                    {'expression': 'ga:users'}
+                  ],
+        'filters':'ga:landingpagepath=~nedvizhimost;ga:pagepath=~services/deal'
+        }
+
+od_users = ga_conc.report_pd(dates_couples,params)
+
+
+
+params = {'dimetions': [{'name': 'ga:isoweek'},
+                        {'name': 'ga:pagepath'},
+                        {'name': 'ga:dimension1'},
+                        {'name': 'ga:dimension2'}
+                       ],
+        'metrics':[
+                    {'expression': 'ga:users'}
+                  ],
+        'filters':'ga:landingpagepath=~nedvizhimost;ga:pagepath=~guaranteed-deal'
+        }
+
+garant_users = ga_conc.report_pd(dates_couples,params)
+
+
+params = {'dimetions': [{'name': 'ga:isoweek'},
+                        {'name': 'ga:eventLabel'},
+                        {'name': 'ga:eventCategory'},
+                        {'name': 'ga:dimension1'}
+                       ],
+        'metrics':[{'expression': 'ga:totalevents'},
+                    {'expression': 'ga:users'}
+                  ],
+        'filters':'ga:landingpagepath=~nedvizhimost;ga:eventLabel=~gd_start'
+        }
+
+garant_lk_banners = ga_conc.report_pd(dates_couples,params)
+
+
+engine = create_engine('sqlite://')
+od_lk_banners.to_sql("OD_LK", con=engine, if_exists = 'replace', index = False)
+od_users.to_sql("OD_USERS", con=engine, if_exists = 'replace', index = False)
+garant_lk_banners.to_sql("GD_LK", con=engine, if_exists = 'replace', index = False)
+garant_users.to_sql("GD_USERS", con=engine, if_exists = 'replace', index = False)
+
+def get_df(query, engine = engine):
+    df_check = engine.execute(query)
+    return pandas.DataFrame(df_check.fetchall(), columns = df_check.keys()).fillna(0)
+
+q = '''
+
+with od_u_pull as 
+(select  isoweek, count(distinct dimension1) AS od_us from OD_USERS where dimension2 != 'not_authorized' GROUP by 1),
+
+gd_u_pull as 
+(select isoweek, count(distinct dimension1) AS gd_us from GD_USERS where dimension2 != 'not_authorized' GROUP by 1),
+
+od_lp_click as
+(select isoweek,  sum(users) as od_clicks from OD_LK GROUP by 1),
+
+gd_lp_click as 
+(select isoweek,  sum(users) as gd_clicks from GD_LK GROUP by 1),
+
+week as 
+(select distinct isoweek as isoweek from GD_USERS)
+
+
+SELECT 
+week.isoweek as isoweek,
+
+od_us,
+gd_us,
+od_clicks,
+gd_clicks
+
+
+FROM week
+
+ LEFT JOIN od_u_pull on 
+od_u_pull.isoweek = week.isoweek
+
+LEFT JOIN gd_u_pull on 
+gd_u_pull.isoweek = week.isoweek
+
+LEFT JOIN od_lp_click on 
+od_lp_click.isoweek = week.isoweek
+
+LEFT JOIN gd_lp_click on 
+gd_lp_click.isoweek = week.isoweek
+
+
+'''
+misc_cross_events = get_df(q)
+
+
+
+rows  = [list([misc_cross_events.columns][0])]
+
+for i in misc_cross_events.itertuples():
+    
+    if i.isoweek == 0:
+        continue
+    else:
+        ls=list(i)[1:]
+        rows.append(ls)
+
+
+sh = gc.open("1. РК Траффик 2020/21 Total")
+wk = sh.worksheet('source_all')
+wk.update('AF2',rows)
