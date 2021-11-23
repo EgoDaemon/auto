c284cb1699a6b06bca0fe23096b05b0cfc7e11f4
Alexandra Messner <54995607+EgoDaemon@users.noreply.github.com>
2021-11-23 17:52:09 +0300

    VAS cost table

---
+++ VAS_by_days.py
@@ -0,0 +1,46 @@
+import pandas_gbq
+import requests
+import datetime
+import json
+import pandas
+from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2 import service_account
+import gspread
+import pandas as pd
+
+SCOPES = ['https://www.googleapis.com/auth/analytics.readonly',
+             'https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name('/home/web_analytics/m2-main-cd9ed0b4e222.json', SCOPES)
+
+
+gc = gspread.authorize(credentials)
+
+sh = gc.open('VAS_по_дням')
+wk = sh.worksheet('list')
+
+list_of_dicts = wk.get_all_records()
+source_tab = pandas.DataFrame(list_of_dicts)
+source_tab
+
+source_tab.columns = [i.replace(' ','_') for i in source_tab.columns]
+source_tab = source_tab[source_tab['timestamp']!='']
+source_tab['start_date'] = source_tab['start_date'].apply(lambda x: datetime.datetime.strptime(x,"%d.%m.%Y" ).date())
+source_tab['daily_earnings'] = source_tab['daily_earnings'].astype(int)
+source_tab['daily_earnings'] = source_tab['daily_earnings'] / 100
+cols = list(source_tab.columns)+['date_spend']
+res = []
+for i in source_tab.itertuples():
+    
+    for date_diff in range(i.days):
+        standart_row = list(i[1:])
+        date_spend = i.start_date + datetime.timedelta(days=date_diff)
+        standart_row.append(date_spend)
+        res.append(standart_row)
+common = pandas.DataFrame(res, columns = cols)
+
+key_path = '/home/web_analytics/m2-main-cd9ed0b4e222.json'
+gbq_credential = service_account.Credentials.from_service_account_file(key_path,)
+
+common.to_gbq(f'sheets.VAS_by_days', project_id='m2-main', if_exists='replace', credentials=gbq_credential)
